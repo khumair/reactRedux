1b37c08f9a6f2002439956b762bb31ea9b5249cf
khumair <khawja.umair@gmail.com>
2018-05-07 22:02:10 +0300

    setup with react and API

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import PostForm from './component/PostForm';
+import Posts from './component/Posts';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <PostForm />
+      <hr />
+      <Posts />
+
+      </div>
+    );
+  }
+}
+
+export default App;
